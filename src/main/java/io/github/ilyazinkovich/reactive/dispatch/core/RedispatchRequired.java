e6c9c63dc38defd005b24dfb23802b82d3f9a57d
Ilya Zinkovich <ilya.zinkovich@careem.com>
2018-11-08 17:36:40 +0100

    Made the booking transient in exception

--- src/main/java/io/github/ilyazinkovich/reactive/dispatch/core/RedispatchRequired.java
+++ src/main/java/io/github/ilyazinkovich/reactive/dispatch/core/RedispatchRequired.java
@@ -2,7 +2,7 @@ package io.github.ilyazinkovich.reactive.dispatch.core;
 
 public class RedispatchRequired extends RuntimeException {
 
-  public final Booking booking;
+  public final transient Booking booking;
 
   public RedispatchRequired(final Booking booking) {
     this.booking = booking;
